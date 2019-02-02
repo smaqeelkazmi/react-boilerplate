c1d26b2016dbc2b1c2908cc2e827628f8b447b88
fahad <bizsoftwebsite@gmail.com>
2019-02-02 22:56:03 +0500

    options for header and footer visibility in routes

--- src/views/_layout/DefaultLayout.jsx
+++ src/views/_layout/DefaultLayout.jsx
@@ -1,29 +1,48 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import routes from "../../routes";
 import DefaultHeader from "./DefaultHeader";
 import DefaultFooter from "./DefaultFooter";
 
+function mapStateToProps(state) {
+    return {
+        state
+    };
+}
+
 class DefaultLayout extends Component {
     render() {
+        const header = <DefaultHeader title="I'm Header" />;
+        const footer = <DefaultFooter/>;
         return (
             <div>
-                <DefaultHeader title="I'm Header" />
                 <Switch>
                     {routes.map((route, idx) => {
-                        return route.component ? (<Route key={idx} path={route.path} exact={route.exact} name={route.name} render={props => (
-                            <route.component {...props} />
-                        )} />)
-                            : (null);
+                        return route.component ? (
+                            <Route key={idx}
+                                   path={route.path}
+                                   exact={route.exact}
+                                   name={route.name}
+                                   render={props => {
+                                       return (
+                                           <div>
+                                               {!route.hasNoHeader ? header : ''}
+                                               <route.component {...props} />
+                                               {!route.hasNoFooter ? footer : ''}
+                                           </div>
+                                       )
+                                   }}
+                            />
+                        ) : (null);
                     },
                     )}
                     {/* <Redirect from="/" to="/dashboard" /> */}
                 </Switch>
-                <DefaultFooter/>
             </div>
         );
     }
 }
 
-export default DefaultLayout;
+export default connect(mapStateToProps)(DefaultLayout);
